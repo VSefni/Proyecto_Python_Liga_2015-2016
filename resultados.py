75a07379d97c492f8ffb10a7649f614be472a064
jon risueño <jrisnav1606@g.ecuaand,es>
2025-12-15 13:35:33 +0100

    Se ha introducido las tres comillas para el help

--- resultados.py
+++ resultados.py
@@ -1,5 +1,10 @@
 
 def quiengana(resultado):
+    '''
+    Funcion que devuelve 1 -1 o 0 dependiendo del resultado del partido que se introduzca
+    :param resultado: el resultado del partido
+    :return: 1, -1 o 0 segun el resultado
+    '''
     goles_local, goles_visitante = map(int,resultado.split("-"))
 
     if goles_local > goles_visitante:
@@ -10,12 +15,22 @@ def quiengana(resultado):
         return 0
 
 def puntos(info):
+    '''
+    Funcion que calcula la puntuacion de un equipo despues de que se le haya pasado los resultados del mismo
+    :param info: las estadisticas del equipo
+    :return: la puntuacion final del equipo
+    '''
     ganados, empatados, perdidos = info
 
     return ganados * 3 + empatados * 1
 
 def infoequipos(datosliga, equipos):
-
+    '''
+    Funcion que recopila toda las estadisticas e informacion del equipo y las devuelve como una lista
+    :param datosliga: los resultados de todos los partidos de la liga
+    :param equipos: el listado de equipos de la liga
+    :return: un listado de equipos de la liga con sus resultados organizados
+    '''
     estadisticas = {}
 
     for equipo in equipos:
